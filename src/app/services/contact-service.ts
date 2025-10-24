0ab3e3673b352721f5cf925815e19f21cc72e431
Demis Moysidis <demis.moysidis@outlook.de>
2025-10-24 11:58:30 +0200

    refactor: adjust color for contact

--- src/app/services/contact-service.ts
+++ src/app/services/contact-service.ts
@@ -130,7 +130,7 @@ export class ContactService implements OnDestroy {
     const contactColors = [
       '#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8',
       '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701',
-      '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B'
+      '#0038FF', '#a5cb45ff', '#FFE62B', '#FF4646', '#FFBB2B'
     ];
     return contactColors[index % contactColors.length];
   }
